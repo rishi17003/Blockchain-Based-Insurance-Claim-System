ebbe02431d2155d5828f26530cf7cf150efb5437
rishi17003 <102285269+rishi17003@users.noreply.github.com>
2024-08-25 17:22:32 +0530

    Create firebase.config.js

---
+++ src/firebase.config.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDodIvoqZH0Ic9Gl6NfHnoIR5YGgUFG6Rc",
+  authDomain: "blockchain-based-insurance.firebaseapp.com",
+  projectId: "blockchain-based-insurance",
+  storageBucket: "blockchain-based-insurance.appspot.com",
+  messagingSenderId: "1018324118035",
+  appId: "1:1018324118035:web:2d1710d5d4ec08ec8a6de6",
+  measurementId: "G-QQPDDCZ5LN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+const auth =getAuth(app)
+export {auth};
