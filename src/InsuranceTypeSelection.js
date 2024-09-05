080b922b04f82138cee3b9a58358ee2cf8579045
rishi17003 <102285269+rishi17003@users.noreply.github.com>
2024-09-05 16:57:28 +0530

    Update InsuranceTypeSelection.js

--- src/InsuranceTypeSelection.js
+++ src/InsuranceTypeSelection.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import './Insurancetype.css';
 import { useNavigate } from 'react-router-dom';
 
 function InsuranceTypeSelection() {
   const navigate = useNavigate();
 
   const handleSelection = (type) => {
-    navigate(`/file-claim/${type.toLowerCase()}`);
+    navigate(`/kyc/${type.toLowerCase()}`);
   };
 
   return (
     <div className="insurance-type-container">
-      <h2>Select Your Insurance Type</h2>
+      <h2>Select Insurance Type</h2>
       <div className="insurance-type-buttons">
-        <button className="btn btn-primary" onClick={() => handleSelection('Vehicle')}>Vehicle Insurance</button>
-        <button className="btn btn-primary" onClick={() => handleSelection('Health')}>Health Insurance</button>
-        <button className="btn btn-primary" onClick={() => handleSelection('Life')}>Life Insurance</button>
-        <button className="btn btn-primary" onClick={() => handleSelection('Travel')}>Travel Insurance</button>
+      <button className="btn btn-primary" onClick={() => handleSelection('Vehicle')}>Vehicle Insurance</button>
+      <button className="btn btn-primary" onClick={() => handleSelection('Health')}>Health Insurance</button>
+      <button className="btn btn-primary" onClick={() => handleSelection('Life')}>Life Insurance</button>
+      <button className="btn btn-primary" onClick={() => handleSelection('Travel')}>Travel Insurance</button>
       </div>
     </div>
   );
