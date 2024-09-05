1beea5423883e89bbdf1fb75879bd663bcf7fe3f
rishi17003 <102285269+rishi17003@users.noreply.github.com>
2024-09-05 16:58:50 +0530

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// import * as firebase from "firebase/app";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -16,9 +17,20 @@ const firebaseConfig = {
   measurementId: "G-QQPDDCZ5LN"
 };
 
+// const app = firebase.initializeApp({
+//     apiKey: "AIzaSyDodIvoqZH0Ic9Gl6NfHnoIR5YGgUFG6Rc",
+//     authDomain: "blockchain-based-insurance.firebaseapp.com",
+//     projectId: "blockchain-based-insurance",
+//     storageBucket: "blockchain-based-insurance.appspot.com",
+//     messagingSenderId: "1018324118035",
+//     appId: "1:1018324118035:web:2d1710d5d4ec08ec8a6de6",
+//     measurementId: "G-QQPDDCZ5LN"
+//   });
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 
-const auth =getAuth(app)
-export {auth};
+export const auth =getAuth(app)
+// auth.settings.appVerificationDisabledForTesting = false;
+export default app;
